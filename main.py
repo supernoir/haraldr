f839b899c7815623002ca8c8d4335da4cdb27847
Conny Kawohl <ckawohl@msxi-euro.com>
2020-02-10 15:55:33 +0100

    updates module

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from bluetooth import discover_devices
+from datetime import datetime
 
-print("performing inquiry...")
+def getCurrentTime():
+     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-nearby_devices = discover_devices(lookup_names = True)
+def printTimeStamp():
+     print("===== %s =====" % getCurrentTime())
 
-print("found %d devices" % len(nearby_devices))
+def discover():
+     printTimeStamp()
 
-for name, addr in nearby_devices:
-     print(" %s - %s" % (addr, name))
+     nearby_devices = discover_devices(duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
+
+     try:
+          if nearby_devices:
+               print("Found %d device(s)" % len(nearby_devices))
+               for addr, name in nearby_devices:
+                    print("Name: %s \t Address: %s" % (name, addr))
+          else:
+               print("No devices found.")
+     except UnicodeEncodeError:
+          print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
+
+
+
+discover()
